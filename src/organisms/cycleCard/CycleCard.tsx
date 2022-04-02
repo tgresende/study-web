fe36e683674cdd7125910ba06546f097ab85a809
tulioresende <tulio.resende.ufmg@gmail.com>
2022-04-02 08:35:19 -0300

    cycle card design

--- src/organisms/cycleCard/CycleCard.tsx
+++ src/organisms/cycleCard/CycleCard.tsx
@@ -1,13 +1,27 @@
-import { Typography } from '@mui/material'
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+} from '@mui/material'
+import { Box } from '@mui/system'
 import { useNavigate } from 'react-router-dom'
 import { pathTypes } from '../../main/paths'
+
+import MyImage from './assets/ciclo-1.jpg'
 
 export default function CycleCard() {
   const navigateTo = useNavigate()
 
   return (
-    <Typography onClick={() => navigateTo(pathTypes.cyclePath)}>
-      outro nome abc
-    </Typography>
+    <Card sx={{ maxWidth: 200 }}>
+      <CardActionArea onClick={() => navigateTo(pathTypes.cyclePath)}>
+        <CardMedia component="img" height="120" image={MyImage} alt="ciclos" />
+        <Box display={`flex`} alignItems={`center`} justifyContent={`center`}>
+          <Typography variant="h5">Ciclos</Typography>
+        </Box>
+      </CardActionArea>
+    </Card>
   )
 }
